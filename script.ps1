392440722e01dc917550abfa4d2ec6abc93ce0b6
Louis Ruocco <louisruocco1@gmail.com>
2023-03-11 19:17:48 +0000

    able to add todos to collection

--- script.ps1
+++ script.ps1
@@ -6,8 +6,14 @@ function getTodos {
     if($todos -eq $null){
         Write-Host "No Tasks added"
     } else {
-        write-host $todos
+        write-host $todos.todo | Format-List
     }
+}
+
+function addTodo {
+    $todo = Read-Host -prompt "Add Task here"
+    @{todo = $todo} | Add-MdbcData
+    Write-Host "$todo Successfully Added!"
 }
 
 getTodos
